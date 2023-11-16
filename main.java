405216e9a8ad12d174fdd8933c1ae8e59fa87761
mohamed hlilou <108653915+hlilou05@users.noreply.github.com>
2023-11-17 02:29:16 +0300

    main.java

--- main.java
+++ main.java
@@ -1,1 +1,68 @@
+import java.util.Arrays;
+
+public class Timsort {
+
+    private static final int MIN_MERGE = 32;
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Array before sorting: " + Arrays.toString(arr));
+
+        // Perform Timsort
+        timsort(arr, arr.length);
+
+        System.out.println("Array after sorting: " + Arrays.toString(arr));
+    }
+
+    public static void timsort(int[] arr, int n) {
+        if (n < 2) {
+            return;
+        }
+
+        int minRun = minRunLength(MIN_MERGE);
+
+        for (int i = 0; i < n; i += minRun) {
+            insertionSort(arr, i, Math.min((i + minRun - 1), (n - 1)));
+        }
+
+        for (int size = minRun; size < n; size = 2 * size) {
+            for (int left = 0; left < n; left += 2 * size) {
+                int mid = left + size - 1;
+                int right = Math.min((left + 2 * size - 1), (n - 1));
+
+                if (mid < right) {
+                    merge(arr, left, mid, right);
+                }
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= left && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        // Merge two subarrays arr[left...mid] and arr[mid+1...right]
+        // Implement your merging logic here
+    }
+
+    public static int minRunLength(int n) {
+        int r = 0;
+        while (n >= MIN_MERGE) {
+            r |= (n & 1);
+            n >>= 1;
+        }
+        return n + r;
+    }
+}
 
